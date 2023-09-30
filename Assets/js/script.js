591ca925a58bd7639bd7abf21dfc463ab604128b
mhdrihan <lifeiscomedy13@gmail.com>
2023-09-30 22:02:00 +0700

    Update Modal, CheckUp Submit

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,49 +1,130 @@
-let hamburger = document.querySelector('.hamburger');
-let navbar = document.querySelector('.navbar');
+let hamburger = document.querySelector(".hamburger");
+let navbar = document.querySelector(".navbar");
 
-hamburger.addEventListener('click', () => {
-    navbar.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("active");
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-        const homeSection = document.querySelector('.home');
-        const surveySection = document.querySelector('section:not(.home)');
+document.addEventListener("DOMContentLoaded", function () {
+  // Ambil elemen-elemen yang akan diubah tampilannya
+  const homeSection = document.querySelector(".home");
+  const surveySection = document.querySelector("section:not(.home)");
 
-        const startTestButton = document.getElementById('start-test');
+  // Ambil tombol "Start Test"
+  const startTestButton = document.getElementById("start-test");
 
-        startTestButton.addEventListener('click', function(event) {
-            event.preventDefault(); 
+  // Tambahkan event listener untuk tombol "Start Test"
+  startTestButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Mencegah tindakan default dari tautan
 
-            homeSection.style.display = 'none';
+    // Sembunyikan section "home"
+    homeSection.style.display = "none";
 
-            surveySection.style.display = 'block';
-        });
+    // Tampilkan section survei
+    surveySection.style.display = "block";
+  });
 });
 
+// Get all the form elements with the class "form-test"
+const forms = document.querySelectorAll(".form-test");
 
-    const forms = document.querySelectorAll('.form-test');
+// Add a change event listener to each radio button in the forms
+forms.forEach((form, index) => {
+  const radioButtons = form.querySelectorAll('input[type="radio"]');
+  radioButtons.forEach((radioButton, radioIndex) => {
+    radioButton.addEventListener("change", () => {
+      // Get the position of the next form
+      if (index < forms.length - 1) {
+        const nextForm = forms[index + 1];
+        const rect = nextForm.getBoundingClientRect();
 
-    forms.forEach((form, index) => {
-        const radioButtons = form.querySelectorAll('input[type="radio"]');
-        radioButtons.forEach((radioButton, radioIndex) => {
-            radioButton.addEventListener('change', () => {
-                if (index < forms.length - 1) {
-                    const nextForm = forms[index + 1];
-                    const rect = nextForm.getBoundingClientRect();
+        // Calculate the center position of the viewport vertically
+        const windowHeight =
+          window.innerHeight || document.documentElement.clientHeight;
+        const centerY = window.scrollY + windowHeight / 2;
 
-                    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-                    const centerY = window.scrollY + windowHeight / 2;
+        // Calculate the scroll position to center the next form vertically
+        const scrollToY =
+          rect.top + window.scrollY - windowHeight / 2 + rect.height / 2;
 
-                    const scrollToY = rect.top + window.scrollY - (windowHeight / 2) + (rect.height / 2);
-
-                    window.scrollTo({
-                        top: scrollToY,
-                        behavior: 'smooth',
-                    });
-                }
-            });
+        // Scroll to the next form with smooth behavior
+        window.scrollTo({
+          top: scrollToY,
+          behavior: "smooth",
         });
+      }
     });
+  });
+});
 
+const carouselSlides = document.querySelectorAll(".carousel-slide");
+let currentIndex = 0;
 
+function showSlide(index) {
+  carouselSlides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = "block";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+}
 
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % carouselSlides.length;
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 7000); // Ganti slide setiap 3 detik
+
+// Tampilkan slide pertama saat halaman dimuat
+showSlide(currentIndex);
+
+function ambilNilai() {
+  const form = document.getElementById("formTest");
+  const modal = document.getElementById("modalHasil");
+  const modalContent = document.getElementById("modalContent");
+  const closeButton = document.querySelector(".close");
+  const submitButton = document.getElementById("submitButton");
+  var jawaban = document.getElementsByName("jawaban");
+  var tampungNilai = [];
+
+  for (var i = 0; i < jawaban.length; i++) {
+    if (jawaban[i].checked) {
+      tampungNilai.push(parseInt(jawaban[i].value));
+    }
+  }
+  // Total Nilai Array
+  var total = tampungNilai.reduce(function (accu, curr) {
+    return accu + curr;
+  }, 0);
+
+  // Penyesuaian Nilai
+  if (tampungNilai.length < 10) {
+    alert("Silahkan Isi Seluruh Jawaban!");
+  } else if (total > 0 && total < 14) {
+    submitButton.addEventListener("click", function () {
+      modalContent.innerHTML = `Level Stress Anda : Low Stress`;
+      modal.style.display = "block";
+    });
+  } else if (total > 14 && total < 27) {
+    submitButton.addEventListener("click", function () {
+      modalContent.innerHTML = `Level Stress Anda : Moderate Stress`;
+      modal.style.display = "block";
+    });
+  } else if (total > 27 && total < 41) {
+    submitButton.addEventListener("click", function () {
+      modalContent.innerHTML = `Level Stress Anda : High Preceived Stress`;
+      modal.style.display = "block";
+    });
+  }
+  closeButton.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+}
